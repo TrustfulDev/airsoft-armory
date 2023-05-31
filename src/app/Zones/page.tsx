169cc92ebad1edcf72c5a6fb21afdb22d9ed35dd
Steven Vu <sssstevenvu@gmail.com>
2023-05-30 18:25:14 -0700

    page: zones & pictures
    Dynamic pages need to be implemented for each zone

--- src/app/Zones/page.tsx
+++ src/app/Zones/page.tsx
@@ -1,8 +1,46 @@
+import styles from './zones.module.css';
+import Image from "next/image";
+
 const zones = () => {
     return (
-        <div>
-            Zones Page
-        </div>
+        <>
+            <h1 className={styles.header}>Choose A Zone</h1>
+            <section className={styles.container}>
+                <div className={styles.box1}>
+                    <Image 
+                        className={styles.pic}
+                        src="/static/zones/gamepodZone.webp"
+                        alt="Gamepod Combat Zone"
+                        fill
+                        loading="lazy"
+                    />
+                    <h2>Gamepod Combat Zone</h2>
+                    
+                </div>
+
+                <div className={styles.box2}>
+                    <Image 
+                        className={styles.pic}
+                        src="/static/zones/cqbCity.webp"
+                        alt="CQB City"
+                        fill
+                        loading="lazy"
+                    />
+                    <h2>CQB City</h2>
+                </div>
+
+                <div className={styles.box3}>
+                    <Image 
+                        className={styles.pic}
+                        src="/static/zones/dsoc.webp"
+                        alt="DSOC Killhouse"
+                        fill
+                        loading="lazy"
+                    />
+                    <h2>DSOC Killhouse</h2>
+                </div>
+            </section>
+        </>
     );
 };
 
